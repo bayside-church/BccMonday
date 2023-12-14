45f20c89e84ab3c602a768343417c819e7f6306e
nlBayside <85907597+nlBayside@users.noreply.github.com>
2023-12-14 12:27:11 -0800

    Add XML docs for Board class

--- Utilities/Api/Board.cs
+++ Utilities/Api/Board.cs
@@ -3,21 +3,31 @@ using System.Collections.Generic;
 
 namespace com.baysideonline.BccMonday.Utilities.Api
 {
+    /// <inheritdoc/>
     public class Board : IBoard
     {
+        /// <inheritdoc/>
         [JsonProperty("id", Required = Required.Always)]
         public long Id { get; set; }
 
+        /// <inheritdoc/>
         [JsonProperty("name", NullValueHandling = NullValueHandling.Ignore)]
         public string Name { get; set; }
 
+        /// <inheritdoc/>
         [JsonProperty("columns", ItemConverterType = typeof(ConcreteConverter<IColumn, Column>), NullValueHandling = NullValueHandling.Ignore)]
         public List<IColumn> Columns { get; set; }
 
         [JsonProperty("items", ItemConverterType = typeof(ConcreteConverter<IItem, Item>), NullValueHandling = NullValueHandling.Ignore)]
         public List<IItem> Items { get; set; }
 
+        /// <summary>
+        /// The board's items (rows).
+        /// </summary>
+        [JsonProperty("items_page")]
+        public string ItemsPage { get; set; }
 
+        /// <inheritdoc/>
         public IColumn GetColumn(string id)
         {
             return Columns?.Find(c => c.Id == id);
--- Utilities/Api/Interfaces/IBoard.cs
+++ Utilities/Api/Interfaces/IBoard.cs
@@ -2,16 +2,33 @@
 
 namespace com.baysideonline.BccMonday.Utilities.Api
 {
+    /// <summary>
+    /// A monday.com board
+    /// </summary>
     public interface IBoard
     {
+        /// <summary>
+        /// The unique identifier of the board.
+        /// </summary>
         long Id { get; set; }
 
+        /// <summary>
+        /// The board's name.
+        /// </summary>
         string Name { get; set; }
 
+        /// <summary>
+        /// The board's visible columns.
+        /// </summary>
         List<IColumn> Columns { get; set; }
 
         List<IItem> Items { get; set; }
 
+        /// <summary>
+        /// Gets a Column by a specified Id.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>IColumn</returns>
         IColumn GetColumn(string id);
     }
 }
