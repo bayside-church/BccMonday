282b6eec08ce63f9769645e94ef49eca7be61da7
Noah Lopez <noah.lopez@baysideonline.com>
2024-03-12 15:50:46 -0700

    MondayItemList updates

--- Blocks/MondayItemList.cs
+++ Blocks/MondayItemList.cs
@@ -1,4 +1,11 @@
-﻿using Rock.Blocks;
+﻿using com.baysideonline.BccMonday.Models;
+using com.baysideonline.BccMonday.Utilities.Api;
+using com.baysideonline.BccMonday.Utilities.Api.Schema;
+using com.baysideonline.BccMonday.ViewModels.MondayItemDetail;
+using Newtonsoft.Json;
+using Rock;
+using Rock.Attribute;
+using Rock.Blocks;
 using Rock.Data;
 using System;
 using System.Collections.Generic;
@@ -20,8 +27,19 @@ namespace com.baysideonline.BccMonday.Blocks
     [SupportedSiteTypes(Rock.Model.SiteType.Web)]
 
     #region Block Attributes
-
-
+    [BooleanField("BccMonday Debug",
+        Name = "Enable Debug Mode",
+        Key = AttributeKey.BccMondayDebug,
+        Description = "Enabling this will generate exceptions.This is very useful for troubleshooting.",
+        IsRequired = true,
+        DefaultBooleanValue = false
+        )]
+    [LavaField("Lava Template",
+        Name = "Lava Template",
+        Key = AttributeKey.LavaTemplate,
+        Description = "The Lava template used to render the list of Monday Items.",
+        IsRequired = false
+        )]
     #endregion
 
     [Rock.SystemGuid.EntityTypeGuid("e46605f9-1fc2-431b-91fe-0d81ea56e2cd")]
@@ -31,6 +49,18 @@ namespace com.baysideonline.BccMonday.Blocks
         public override string ObsidianFileUrl => $"/Plugins/com_baysideonline/BccMonday/Blocks/MondayItemList.obs";
 
         #region Keys
+        private static class AttributeKey
+        {
+            public const string BccMondayDebug = "BccMondayDebug";
+            public const string LavaTemplate = "LavaTemplate";
+        }
+
+        private static class PageParameterKey
+        {
+            public const string BccMondayBoardId = "BccMondayBoardId";
+            public const string MondayItemId = "MondayItemId";
+            public const string SelectedBoard = "SelectedBoard";
+        }
 
         #endregion Keys
 
@@ -39,12 +69,111 @@ namespace com.baysideonline.BccMonday.Blocks
         /// <inheritdoc/>
         public override object GetObsidianBlockInitialization()
         {
-            using (var rockContext = new RockContext())
-            {
-                var box = 1;// new MondayItemListBox();
+            var requestorEmail = RequestContext.CurrentPerson.Email;
 
-                return box;
+            //Default to the open board
+            var boardOption = PageParameter(PageParameterKey.SelectedBoard ?? "Open").Equals("Closed", StringComparison.OrdinalIgnoreCase);
+            var bccMondayBoardId = PageParameter(PageParameterKey.BccMondayBoardId).AsIntegerOrNull();
+            var bccMondayBoard = GetBccMondayBoard(bccMondayBoardId);
+
+            var chosenBoard = GetChosenBoard(bccMondayBoard, boardOption);
+            var api = new MondayApi();
+
+            List<ItemsPageByColumnValuesQuery> columnValuesQuery = new List<ItemsPageByColumnValuesQuery>
+                {
+                    new ItemsPageByColumnValuesQuery
+                    {
+                        ColumnId = bccMondayBoard.EmailMatchColumnId,
+                        ColumnValues = new List<string> { requestorEmail }
+                    }
+                };
+            var items = api.GetItemsByBoardAndColumnValues(chosenBoard.Value, columnValuesQuery);
+            List<MondayItemBag> itemsBag = new List<MondayItemBag>();
+            foreach (var item in items)
+            {
+                itemsBag.Add(CreateBag(item));
             }
+            //var result = JsonConvert.SerializeObject(items);
+
+            var lavaTemplate = GetAttributeValue(AttributeKey.LavaTemplate);
+            var mergeFields = new Dictionary<string, object>
+            {
+                { "items", JsonConvert.SerializeObject(itemsBag) }
+            };
+
+            var renderedHtml = lavaTemplate.ResolveMergeFields(mergeFields);
+
+            return new
+            {
+                Content = renderedHtml
+            };
+        }
+
+        public BccMondayBoard GetBccMondayBoard(int? Id)
+        {
+            if (!Id.HasValue)
+            {
+                return null;
+            }
+
+            var bccMondayBoardService = new BccMondayBoardService(new RockContext());
+            return bccMondayBoardService.Get(Id.Value) ?? null;
+        }
+
+        public long? GetChosenBoard(BccMondayBoard board, bool closed = false)
+        {
+            if (closed)
+            {
+                return board.MondayBoardId;
+            }
+
+            board.LoadAttributes();
+            var closedBoardId = board.GetAttributeValue("MondayClosedBoardId");
+            return closedBoardId.IsNotNullOrWhiteSpace() ? long.Parse(closedBoardId) : board.MondayBoardId;
+        }
+
+        public MondayItemBag CreateBag(Item item)
+        {
+            var itemBag = new MondayItemBag
+            {
+                Id = item.Id.ToString(),
+                Name = item.Name,
+                CreatedAt = item.CreatedAt.ToString(),
+                ColumnValues = item.ColumnValues.Select(c =>
+                {
+                    MondayColumnValueBag mondayColumnValueBag = new MondayColumnValueBag
+                    {
+                        Id = c.ColumnId,
+                        Text = c.Text,
+                        Type = c.Type,
+                        Value = c.Value,
+                        Column = new MondayColumnBag
+                        {
+                            Id = c.Column.Id,
+                            Title = c.Column.Title,
+                        }
+                    };
+
+                    // Check the type and assign properties accordingly
+                    if (c is StatusColumnValue statusColumnValue)
+                    {
+                        //mondayColumnValueBag.Index = statusColumnValue.Index;
+                        //mondayColumnValueBag.StatusLabel = statusColumnValue.StatusLabel;
+                        //mondayColumnValueBag.IsDone = statusColumnValue.IsDone;
+                        mondayColumnValueBag.LabelStyle = new LabelStyle
+                        {
+                            Color = statusColumnValue.LabelStyle.Color,
+                            Border = statusColumnValue.LabelStyle.Border
+                        };
+                    }
+                    
+                    return mondayColumnValueBag;
+                }).ToList()
+            };
+
+            var foo = Newtonsoft.Json.JsonConvert.SerializeObject(itemBag.ColumnValues);
+
+            return itemBag;
         }
 
         #endregion
--- dist/mondayItemList.obs.js
+++ dist/mondayItemList.obs.js
@@ -1,12 +1,41 @@
-System.register([], (function (exports) {
-	'use strict';
-	return {
-		execute: (function () {
+System.register(['vue', '@Obsidian/Utility/block'], (function (exports) {
+  'use strict';
+  var defineComponent, openBlock, createElementBlock, unref, useConfigurationValues, useInvokeBlockAction, useReloadBlock, onConfigurationValuesChanged;
+  return {
+    setters: [function (module) {
+      defineComponent = module.defineComponent;
+      openBlock = module.openBlock;
+      createElementBlock = module.createElementBlock;
+      unref = module.unref;
+    }, function (module) {
+      useConfigurationValues = module.useConfigurationValues;
+      useInvokeBlockAction = module.useInvokeBlockAction;
+      useReloadBlock = module.useReloadBlock;
+      onConfigurationValuesChanged = module.onConfigurationValuesChanged;
+    }],
+    execute: (function () {
 
-			var script = exports('default', {});
-			script.__file = "src/mondayItemList.obs";
+      var _hoisted_1 = ["innerHTML"];
+      var script = exports('default', defineComponent({
+        name: 'mondayItemList',
+        setup(__props) {
+          var config = useConfigurationValues();
+          useInvokeBlockAction();
+          var reloadBlock = useReloadBlock();
+          console.log("Config from block:\n ", config);
+          var content = config.content;
+          onConfigurationValuesChanged(reloadBlock);
+          return (_ctx, _cache) => {
+            return openBlock(), createElementBlock("div", {
+              innerHTML: unref(content)
+            }, null, 8, _hoisted_1);
+          };
+        }
+      }));
 
-		})
-	};
+      script.__file = "src/mondayItemList.obs";
+
+    })
+  };
 }));
 //# sourceMappingURL=mondayItemList.obs.js.map
