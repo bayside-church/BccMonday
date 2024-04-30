f76831c4cbd92e0efa5771fe547b7e3e3eed4470
Noah Lopez <noah.lopez@baysideonline.com>
2024-04-30 14:01:45 -0700

    Refactor CreateItem

--- Workflows/CreateItem.cs
+++ Workflows/CreateItem.cs
@@ -1,17 +1,15 @@
 ﻿using Rock.Data;
 using Rock.Model;
 using Rock.Workflow;
-using System;
 using System.Collections.Generic;
 using System.ComponentModel.Composition;
 using System.ComponentModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Rock.Attribute;
-using com.baysideonline.BccMonday.Utilities.Api;
 using Rock;
 using Newtonsoft.Json;
+using com.baysideonline.BccMonday.Utilities.Api;
+using com.baysideonline.BccMonday.Utilities.Api.Config;
 
 namespace com.baysideonline.BccMonday.Workflows
 {
@@ -48,10 +46,16 @@ namespace com.baysideonline.BccMonday.Workflows
         Order = 3
         )]
     [KeyValueListField(
+        "Requester",
+        Description = "The requester(s) who should view the new Item.",
+        Key = "Requester",
+        Order = 4
+        )]
+    [KeyValueListField(
         "Column Values",
         Description = "The column values of the new Item. The keys must all be lowercase.",
         Key = "ColumnValues",
-        Order = 4
+        Order = 5
         )]
     [WorkflowAttribute(
         "Item Id Attribute",
@@ -60,7 +64,7 @@ namespace com.baysideonline.BccMonday.Workflows
         "",
         "",
         Key = "ItemIdResultAttribute",
-        Order = 5
+        Order = 6
         )]
     [WorkflowAttribute(
         "Item Attribute",
@@ -69,7 +73,7 @@ namespace com.baysideonline.BccMonday.Workflows
         "",
         "",
         Key = "ItemResultAttribute",
-        Order = 6
+        Order = 7
         )]
     [BooleanField(
         "Continue on Error",
@@ -77,7 +81,7 @@ namespace com.baysideonline.BccMonday.Workflows
         false,
         "",
         Key = "ContinueOnError",
-        Order = 7
+        Order = 8
         )]
 
 
@@ -86,8 +90,40 @@ namespace com.baysideonline.BccMonday.Workflows
         public override bool Execute(RockContext rockContext, WorkflowAction action, object entity, out List<string> errorMessages)
         {
             errorMessages = new List<string>();
-
             var mergeFields = GetMergeFields(action);
+            var options = GetItemCreationOptions(action, mergeFields, out errorMessages);
+
+            if (!TryCreateItem(options, out var item, out var error))
+            {
+                errorMessages.Add(error);
+                action.AddLogEntry(error);
+                if (!GetAttributeValue(action, "ContinueOnError").AsBoolean())
+                {
+                    return false;
+                }
+
+                return true;
+            }
+
+            action.AddLogEntry($"Monday.com Item({item.Id}) has been created.");
+            var itemId = SetWorkflowAttributeValue(action, "ItemIdResultAttribute", item.Id);
+            var itemResult = SetWorkflowAttributeValue(action, "ItemResultAttribute", JsonConvert.SerializeObject(item));
+
+            if (itemId != null)
+            {
+                action.AddLogEntry($"Set {itemId.Name} attribute to {item.Id}");
+            }
+
+            if (itemResult != null)
+            {
+                action.AddLogEntry($"Set {itemResult.Name} attribute to {JsonConvert.SerializeObject(item)}");
+            }
+            return true;
+        }
+
+        private ItemCreationOptions GetItemCreationOptions(WorkflowAction action, Dictionary<string, object> mergeFields, out List<string> errorMessages)
+        {
+            errorMessages = new List<string>();
             var name = GetAttributeValue(action, "Name", true).ResolveMergeFields(mergeFields);
             var boardId = GetAttributeValue(action, "BoardId", true).ResolveMergeFields(mergeFields);
             var groupId = GetAttributeValue(action, "GroupId", true).ResolveMergeFields(mergeFields);
@@ -98,9 +134,27 @@ namespace com.baysideonline.BccMonday.Workflows
                 .Where(c => c.Value.IsNotNullOrWhiteSpace())
                 .ToDictionary(k => k.Key, k => k.Value);
 
-            var serializedCV = JsonConvert.SerializeObject(columnValues);
+            var requesterValue = GetAttributeValue(action, "Requester", true).ResolveMergeFields(mergeFields).AsDictionaryOrNull();
+            var missingRequester = "Missing requester values";
 
-            var options = new Utilities.Api.Config.ItemCreationOptions
+            if (requesterValue == null || requesterValue.FirstOrDefault().Value.IsNullOrWhiteSpace())
+            {
+                action.AddLogEntry(missingRequester);
+                errorMessages.Add(missingRequester);
+            }
+
+            var requesterValues = requesterValue.FirstOrDefault().Value.Split(',').Select(v => v.Trim()).ToList();
+
+            if (requesterValues.Count < 1)
+            {
+                action.AddLogEntry(missingRequester);
+                errorMessages.Add(missingRequester);
+            }
+            var requesterFormat = string.Join("|", requesterValues);
+            requesterFormat = $"|{requesterFormat}|";
+            columnValues.Add(requesterValue.FirstOrDefault().Key, requesterFormat);
+
+            var options = new ItemCreationOptions
             {
                 Name = name,
                 BoardId = long.Parse(boardId),
@@ -109,41 +163,21 @@ namespace com.baysideonline.BccMonday.Workflows
                 CreateLabelsIfMissing = false
             };
 
+            return options;
+        }
+
+        private bool TryCreateItem(ItemCreationOptions options, out Utilities.Api.Schema.Item item, out string error)
+        {
             var api = new MondayApi();
+            item = api.CreateItem(options);
 
-            var item = api.CreateItem(options);
-
-            if (item != null)
+            if (item == null)
             {
-                action.AddLogEntry($"Monday.com Item({ item.Id }) has been created.");
-                var itemId = SetWorkflowAttributeValue(action, "ItemIdResultAttribute", item.Id);
-                var itemResult = SetWorkflowAttributeValue(action, "ItemResultAttribute", JsonConvert.SerializeObject(item));
-
-                if (itemId != null)
-                {
-                   action.AddLogEntry($"Set {itemId.Name} attribute to {item.Id}");
-                }
-
-                if (itemResult != null)
-                {
-                    action.AddLogEntry($"Set {itemResult.Name} attribute to {JsonConvert.SerializeObject(item)}");
-                }
-                return true;
+                error = "Could not create Monday.com Item";
+                return false;
             }
-            else
-            {
-                var error = "Could not create Monday.com Item";
-                action.AddLogEntry(error);
-                errorMessages.Add(error);
-
-                if (!GetAttributeValue(action, "ContinueOnError").AsBoolean())
-                {
-                    return false;
-                }
-
-                return true;
-
-            }
+            error = null;
+            return true;
         }
     }
 }
